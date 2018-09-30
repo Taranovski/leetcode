556e7fc3b48c64b6830b5263e49db1ccd61f0c0a
Alyx <Taranovski@yandex.ru>
2018-09-29 21:53:19 -0400

    added the solution to the task 916

---
+++ src/main/java/com/taranovski/leetcode/solutions/hard/failing/leetcode0004/Solution.java
@@ -0,0 +1,10 @@
+package com.taranovski.leetcode.solutions.hard.failing.leetcode0004;
+
+/**
+ * Created by Alyx on 29.09.2018.
+ */
+public class Solution {
+//    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+//
+//    }
+}
---
+++ src/main/java/com/taranovski/leetcode/solutions/medium/leetcode0916/Solution.java
@@ -0,0 +1,77 @@
+package com.taranovski.leetcode.solutions.medium.leetcode0916;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Alyx on 29.09.2018.
+ */
+public class Solution {
+    public List<String> wordSubsets(String[] A, String[] B) {
+
+        Map<Character, Integer> globalRequirements = new HashMap<>();
+
+        for (String s : B) {
+            Map<Character, Integer> requirements = getRequirements(s);
+            mergeToGlobalRequirements(globalRequirements, requirements);
+        }
+
+        List<String> result = new ArrayList<>();
+
+        for (String s : A) {
+            if (conformsToRequirements(s, globalRequirements)) {
+                result.add(s);
+            }
+        }
+
+        return result;
+    }
+
+    private boolean conformsToRequirements(String s, Map<Character, Integer> globalRequirements) {
+        final Map<Character, Integer> requirements = getRequirements(s);
+
+        for (Map.Entry<Character, Integer> entry : globalRequirements.entrySet()) {
+            final Character key = entry.getKey();
+            if (!requirements.containsKey(key)) {
+                return false;
+            } else {
+                final Integer globalCounter = entry.getValue();
+                final Integer currentCounter = requirements.get(key);
+
+                if (currentCounter < globalCounter) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private void mergeToGlobalRequirements(Map<Character, Integer> globalRequirements, Map<Character, Integer> requirements) {
+        for (Map.Entry<Character, Integer> entry : requirements.entrySet()) {
+            final Character key = entry.getKey();
+            final Integer value = entry.getValue();
+            if (globalRequirements.containsKey(key)) {
+                final Integer globalCounter = globalRequirements.get(key);
+                if (value > globalCounter) {
+                    globalRequirements.put(key, value);
+                }
+            } else {
+                globalRequirements.put(key, value);
+            }
+        }
+    }
+
+    private Map<Character, Integer> getRequirements(String s) {
+        Map<Character, Integer> requirements = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            if (!requirements.containsKey(c)) {
+                requirements.put(c, 0);
+            }
+            requirements.put(c, requirements.get(c) + 1);
+        }
+        return requirements;
+    }
+}
